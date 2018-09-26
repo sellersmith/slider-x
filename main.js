91fb7b8e920751878d03d0c1f889e1db896815b7
Huỳnh Tuấn Anh <anhht218@Huynhs-MacBook-Pro.local>
2018-09-26 10:52:05 +0700

    Added moveSlide func

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 class SliderController {
   constructor(ele, opts) {
-    console.log('init new slider', ele)
     this.el = ele
     this.$el = $(this.el)
     this.totalChild = this.$el.children().length
+
     this.opts = opts
+
+    this.autoPlayIntervalId = ''
     this.active = false
     this.curr = 0
     this.initialize(this.el, this.opts)
   }
 
   initialize() {
+    console.log("Init new PF Slider")
+
     const currIndex = this.curr
     this.$el.addClass('slider')
 
@@ -24,59 +28,55 @@ class SliderController {
     $('<button>').text('Next').insertAfter(this.el).click(() => this.next.apply(this))
     $('<button>').text('Prev').insertAfter(this.el).click(() => this.prev.apply(this))
 
+
+    if (this.opts.autoPlay) {
+      console.log('auto play on')
+      this.autoPlayIntervalId = setInterval(() => this.moveSlide('next'), 3000)
+    }
+
     return this
   }
 
-  async next() {
-    if (this.active) {
-      // console.log('return')
-      return
-    }
-    
-
+  next() {
+    if (this.active) return
     this.active = true
+    this.moveSlide("next")
+  }
 
+  prev() {
+    if (this.active) return
+    this.active = true
+    this.moveSlide("prev")
+  }
+
+  moveSlide(direction) {
     let currIndex = this.curr
-    let nextIndex = currIndex === (this.totalChild - 1) ? 0 : currIndex + 1
+    let nextIndex, nextSlidePos, currSlidePos
+
+    if (direction === "next") {
+      nextIndex = currIndex === (this.totalChild - 1) ? 0 : currIndex + 1
+      nextSlidePos = '100%'
+      currSlidePos = '-100%'
+    } else if (direction === "prev") {
+      nextIndex = currIndex === 0 ? (this.totalChild - 1) : currIndex - 1
+      nextSlidePos = '-100%'
+      currSlidePos = '100%'
+    }
 
     let $curr = this.$el.children().eq(currIndex)
     let $next = this.$el.children().eq(nextIndex)
 
     $('.animation').removeClass('animation')
+    $next.css('left', nextSlidePos)
 
-    $next.css('left', '100%')
-
-    await setTimeout(() => {
-      $curr.addClass('animation').css('left', '-100%')
+    setTimeout(() => {
+      $curr.addClass('animation').css('left', currSlidePos)
       $next.addClass('animation').css('left', '0')
       setTimeout(() => {
         this.active = false
       }, 400)
-    }, 10)
+    }, 20)
 
     this.curr = nextIndex
-  }
-
-  prev() {
-    if (this.active) {
-      this.active = false
-      let currIndex = this.curr
-      let prevIndex = currIndex === 0 ? (this.totalChild - 1) : currIndex - 1
-  
-      let $curr = this.$el.children().eq(currIndex)
-      let $prev = this.$el.children().eq(prevIndex)
-  
-      $('.animation').removeClass('animation')
-  
-      $prev.css('left', '-100%')
-  
-      setTimeout(() => {
-        $curr.addClass('animation').css('left', '100%')
-        $prev.addClass('animation').css('left', '0')
-      }, 10)
-  
-      this.curr = prevIndex
-    }
-    this.active = true
   }
 }
